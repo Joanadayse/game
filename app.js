fa1ef6913ceec85fb684f0c1823385a8cc090577
Joanadayse <daysejoana3@gmail.com>
2024-07-08 15:05:32 -0300

    iniciando com javaScript

---
+++ app.js
@@ -0,0 +1,8 @@
+alert("Boas-vindas ao jogo do Número Secreto");
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+
+if (chute == numeroSecreto) {
+  console.log("Isso aí! você descobriu o número secreto (5)");
+}
+
